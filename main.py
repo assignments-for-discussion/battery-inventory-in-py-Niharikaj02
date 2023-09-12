82f6e57d9953a583f03ca7ea1293fafaf37ff975
Niharika J <77984285+Niharikaj02@users.noreply.github.com>
2023-09-12 12:44:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 
 def count_batteries_by_health(present_capacities):
-  return {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
-  }
+  battery_health = {}
+  battery_health.setdefault('healthy',0)
+  battery_health.setdefault('exchange',0)
+  battery_health.setdefault('failed',0)
+  
+  for i in present_capacities:
+    SOH = calcSOH(i)
+    if SOH < 63.0:
+      battery_health['failed']+=1
+    elif SOH < 80.0:
+      battery_health['exchange']+=1
+    else:
+      battery_health['healthy']+=1
 
+  return battery_health
+
+def calcSOH(n):
+  return 100*n/120
 
 def test_bucketing_by_health():
   print("Counting batteries by SoH...\n")
