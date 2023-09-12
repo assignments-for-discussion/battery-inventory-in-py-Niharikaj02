508f1c369c3e289934a33fdd0511c5390ea67ea2
Niharika J <77984285+Niharikaj02@users.noreply.github.com>
2023-09-12 13:30:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ exchange_threshold = 80.0
 
 def count_batteries_by_health(present_capacities):
     #initialize default value to 0
-    battery_health = defaultdict(int)
+    health_classification_count = defaultdict(int)
     for capacity in present_capacities:
         #to ensure each capacity is within the range
         assert(capacity>=0) 
         assert(capacity<=120)
         SOH = 100*capacity/120
         if SOH < fail_threshold:
-            battery_health["failed"]+=1
+            health_classification_count["failed"]+=1
         elif SOH < exchange_threshold:
-            battery_health["exchange"]+=1
+            health_classification_count["exchange"]+=1
         else:
-            battery_health["healthy"]+=1
-    return battery_health
+            health_classification_count["healthy"]+=1
+    return health_classification_count
 
 def test_bucketing_by_health():
     print("Counting batteries by SoH...\n")
